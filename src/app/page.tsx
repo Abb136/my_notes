4d828b81d1327ef40d65fea04cbabda79399f90e
Traner110 <117926723+Traner110@users.noreply.github.com>
2025-07-20 19:03:38 +0300

    jn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import{ getClient }from "./lib/mongodb";
 
 export default async function Home() {
   const client = getClient();
-  const data = await client.db('testing').collection<{note:string}>('dataBase').find().limit(50).toArray();
+  const data = await client.db('testing').collection<{note:string}>('dataBase').find().toArray();
   return (<>
-  <Submit_1/>
+  {/* <Submit_1/> */}
   {data.map((i)=><Submit key={i.note} note={i.note}/>)}
-  </>
-  );
+  </>);
 }
